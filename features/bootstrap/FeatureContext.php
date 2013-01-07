d3f85c1ccdbcffd9dfcc7d6ed91f9508403ff305
mopad <mopad@nside.cc>
2013-01-07 22:26:56 +0100

    clean up code behind

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -12,16 +12,13 @@ use Behat\MinkExtension\Context\MinkContext;
 //
 // Require 3rd-party libraries here:
 //
-//   require_once 'PHPUnit/Autoload.php';
    require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
 
 /**
  * Features context.
  */
 class FeatureContext extends MinkContext
 
-//class FeatureContext extends BehatContext
 {
     /**
      * Initializes context.
@@ -34,24 +31,10 @@ class FeatureContext extends MinkContext
         // Initialize your context here
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
     public function jqueryWait($duration = 1000)
     {
-//        $this->getSession()->wait($duration, '');
-        $this->getSession()->wait($duration, "(jQuery.active)");
-//        $this->getSession()->wait($duration, '(0 === jQuery.active)');
+        $this->getSession()->wait($duration, '');
     }
-
 
     /**
      * @Then /^I should see the modal "([^"]*)"$/
@@ -61,7 +44,6 @@ class FeatureContext extends MinkContext
         $this->jqueryWait(4000);
         $this->assertElementContainsText('#myModal h3', $arg1);
         assertTrue($this->getSession()->getPage()->find('css', '#myModal')->isVisible());
-//        throw new PendingException();
     }
 
 }
